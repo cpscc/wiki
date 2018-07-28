01496fc6bb82c7124ad7e5ec80f5af3b42816aa9
Brian <anoxic@users.noreply.github.com>
2018-07-27 19:48:46 -0500

    create a java example request

---
+++ transact-example.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+
+class CornerstoneTransactionExample {
+	public static void main(String[] args) {
+		String url = "https://api.cornerstone.cc/v1/transactions";
+		String user = "sandbox_3xSOjtxSvICXVOKYqbwI";
+		String key = "key_RdutJGqI50YIwjehGtHBOe1Uu";
+		String result = "";
+		
+		String request = 
+			"amount=15&card[number]=4444333322221111&card[expmonth]=12&card[expyear]=23" +
+			"&customer[firstname]=Robert&customer[lastname]=Parr&customer[email]=robertp@example.com";
+
+		try {				
+			HttpURLConnection con = (HttpURLConnection) (new URL(url)).openConnection();
+			
+			byte[] message = (user+":"+key).getBytes("UTF-8");
+			String encoded = javax.xml.bind.DatatypeConverter.printBase64Binary(message);
+						
+			con.setRequestProperty("User-Agent", user);
+			con.setRequestProperty("Authorization", "Basic " + encoded);
+			con.setRequestMethod("POST");
+			con.setDoOutput(true);
+			
+			DataOutputStream out = new DataOutputStream(con.getOutputStream());
+			out.writeBytes(request);
+			out.flush();
+			out.close();
+			
+			InputStream stream;
+			if (con.getResponseCode() < 400) {
+				stream = con.getInputStream();
+			} else {
+				stream = con.getErrorStream();
+			}
+						
+			BufferedReader buf = new BufferedReader(new InputStreamReader(stream));
+			
+			String line;
+			while ((line = buf.readLine()) != null) {
+				result += line + "\n";
+			}
+
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
