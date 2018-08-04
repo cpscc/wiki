a390547a3a0ccafcf1737a6eac9c20ad585a59b9
myklv <bzick@cstonemail.com>
2018-08-04 15:51:16 +0000

    convert basic "hello" json in raw-form request

--- build/src/Spec/Compile.php
+++ build/src/Spec/Compile.php
@@ -1,10 +1,20 @@
 <?php
 namespace Cornerstone\Quarry\Spec;
+use Cornerstone\Quarry\Targets\RawForm;
 
 class Compile
 {
-    function cli()
+    function cli($args)
     {
-        echo "TODO: open json file, compile to an example code\n";
+        if ($args) {
+            $files = [$args[0]];
+        } else {
+            $files = glob("../spec/*.json");
+        }
+
+        foreach ($files as $f) {
+            $f = json_decode(file_get_contents($f), 1);
+            RawForm::convert($f);
+        }
     }
 }
