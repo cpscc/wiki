dde15c57537158ce44079d13d82c81a240df4c29
myklv <bzick@cstonemail.com>
2018-08-04 15:21:40 +0000

    shift $argv to make simpler command input

--- build/src/Spec.php
+++ build/src/Spec.php
@@ -5,14 +5,17 @@ class Spec
 {
     function cli($argc, $argv)
     {
+        $script = array_shift($argv);
+        $command = array_shift($argv);
+
         if ($argc < 2) {
             echo "ERROR: at least 1 argument is required\n";
             exit(1);
         }
 
-        $class = 'Cornerstone\\Quarry\\Spec\\' . ucwords($argv[1]);
+        $class = 'Cornerstone\\Quarry\\Spec\\' . ucwords($command);
         if (class_exists($class)) {
-            (new $class)->cli($argc, $argv);
+            (new $class)->cli($argv);
         } else {
             echo "ERROR: unknown sub-command \"$argv[1]\"\n";
             exit;
