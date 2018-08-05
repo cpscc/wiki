da1822594f18813c3aba0e1477b167e8f0d4fa5f
myklv <bzick@cstonemail.com>
2018-08-05 16:19:28 +0000

    write compiled requests to build/work/

--- build/src/Spec/Compile.php
+++ build/src/Spec/Compile.php
@@ -12,9 +12,12 @@ class Compile
             $files = glob("../spec/*.json");
         }
 
-        foreach ($files as $f) {
-            $f = json_decode(file_get_contents($f), 1);
-            RawForm::convert($f, $config);
+        foreach ($files as $fname) {
+            $f = json_decode(file_get_contents($fname), 1);
+            $name = pathinfo($fname)['filename'];
+            $path = "work/$name." . RawForm::name . ".request";
+            $out = RawForm::convert($f, $config);
+            file_put_contents($path, $out);
         }
     }
 }
--- build/src/Targets/RawForm.php
+++ build/src/Targets/RawForm.php
@@ -3,11 +3,16 @@ namespace Cornerstone\Quarry\Targets;
 
 class RawForm
 {
+    const name = "raw.x-www-form-urlencoded";
+
     function convert(array $spec, array $config)
     {
         $config = $config[$spec['version']];
-        echo "$spec[method] $config[path]$spec[path] HTTP/1.1\n";
-        echo "Host: $config[endpoint]\n";
-        echo "Accept: application/x-www-form-urlencoded\n";
+        $out = "";
+        $out.= "$spec[method] $config[path]$spec[path] HTTP/1.1\n";
+        $out.= "Host: $config[endpoint]\n";
+        $out.= "Accept: application/x-www-form-urlencoded\n";
+        return $out;
     }
 }
+// TODO: use a $handle instead of a string
