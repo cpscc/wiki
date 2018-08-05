824dd7512c8d68e6d09374890a56018c18f201bb
myklv <bzick@cstonemail.com>
2018-08-05 16:08:43 +0000

    rename $json to $spec in Targets/*::convert

--- build/src/Targets/RawForm.php
+++ build/src/Targets/RawForm.php
@@ -3,10 +3,10 @@ namespace Cornerstone\Quarry\Targets;
 
 class RawForm
 {
-    function convert(array $json, array $config)
+    function convert(array $spec, array $config)
     {
-        $config = $config[$json['version']];
-        echo "$json[method] $config[path]$json[path] HTTP/1.1\n";
+        $config = $config[$spec['version']];
+        echo "$spec[method] $config[path]$spec[path] HTTP/1.1\n";
         echo "Host: $config[endpoint]\n";
         echo "Accept: application/x-www-form-urlencoded\n";
     }
