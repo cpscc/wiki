339164150a23c508d76a9eebd6889ea99d93017d
myklv <bzick@cstonemail.com>
2018-08-07 19:12:21 +0000

    raw form target supports request body

--- build/src/Targets/RawForm.php
+++ build/src/Targets/RawForm.php
@@ -11,5 +11,12 @@ class RawForm
         fwrite($h, "$spec[method] $config[path]$spec[path] HTTP/1.1\n");
         fwrite($h, "Host: $config[endpoint]\n");
         fwrite($h, "Accept: application/x-www-form-urlencoded\n");
+
+        if ($spec['body']) {
+            $body = http_build_query($spec['body']);
+            fwrite($h, "Content-Type: application/x-www-form-urlencoded\n");
+            fwrite($h, "Content-Length: " . strlen($body) . "\n\n");
+            fwrite($h, $body . "\n");
+        }
     }
 }
