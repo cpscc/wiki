136d2dd348dd50b319dafed2c06ebe83e1c94cd4
myklv <bzick@cstonemail.com>
2018-08-07 19:20:36 +0000

    allow overriding default config with env vars

--- build/src/Spec.php
+++ build/src/Spec.php
@@ -7,8 +7,7 @@ class Spec
     {
         $script = array_shift($argv);
         $command = array_shift($argv);
-        $config = parse_ini_file('../defaults.ini', true);
-        // TODO: replace defaults with ENV if present
+        $config = $this->config();
 
         if ($argc < 2) {
             echo "ERROR: at least 1 argument is required\n";
@@ -22,5 +21,15 @@ class Spec
             echo "ERROR: unknown sub-command \"$argv[1]\"\n";
             exit(1);
         }
+    }
+
+    function config()
+    {
+        $config = parse_ini_file('../defaults.ini', true);
+        $config['v1']['user'] = getenv('V1_USER') ?: $config['v1']['user'];
+        $config['v1']['pass'] = getenv('V1_PASS') ?: $config['v1']['pass'];
+        $config['v1']['endpoint'] = getenv('V1_ENDPOINT') ?: $config['v1']['endpoint'];
+        $config['v1']['path'] = getenv('V1_PATH') ?: $config['v1']['path'];
+        return $config;
     }
 }
