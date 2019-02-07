c2485fb31b19b5b825ac71b7b4ffc14841e384c9
myklv <+@myklv.com>
2019-02-06 21:36:52 -0600

    cleanup php example

--- transact-example.php
+++ transact-example.php
@@ -1,15 +1,4 @@
 <?php
-# Sample Code:
-#
-# This sample creates a simple call to the Cornerstone API to create a transaction.
-# client id/key are provided by Cornestone Payment Systems.
-#
-# Credentials for Test Cards
-# Securenet: 4444333322221111 / 12 24 / 123
-# Sage:		 4111111111111111 / 12 24 / 123
-#
-# For the id and key include the entire string give.
-
 $data = array(
 		  "amount" => "15",
 		  "customer" => array(
@@ -31,14 +20,12 @@ $key 	 = "key_RdutJGqI50YIwjehGtHBOe1Uu";
 $request = http_build_query($data);
 
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $request);		
+curl_setopt($ch, CURLOPT_USERPWD, "$id:$key");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
 
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);		
-	curl_setopt($ch, CURLOPT_USERPWD, "$id:$key");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    var_export($response);
+var_export($response);
