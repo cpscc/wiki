cab42b93bceef2366f07f7cd0e35d921b8f97eff
Bad Sir Brian <anoxic@users.noreply.github.com>
2020-07-21 13:53:14 -0500

    cleanup c# example, using xml instead

--- transact-example.cs
+++ transact-example.cs
@@ -1,73 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
 using System.Net;
-using System.Threading.Tasks;
 using System.IO;
-/*
-*  Cornerstone API - Create Transaction Example.
-*
-*  06/13/2016
-*/
-namespace CornerstoneCreate
+
+namespace CornerstoneTransactExample
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Initializing..");
-            // Define URL and Credentials
-            string url   = "https://api.cornerstone.cc/v1/transactions";
-            string user  = "sandbox_3xSOjtxSvICXVOKYqbwI";
-            string key   = "key_RdutJGqI50YIwjehGtHBOe1Uu";
-            string responseString = "";
+	class Program
+	{
+		static void Main()
+		{
+			string url   = "https://api.cornerstone.cc/v1/transactions";
+			string user  = "sandbox_3xSOjtxSvICXVOKYqbwI";
+			string key   = "key_RdutJGqI50YIwjehGtHBOe1Uu";
 
-            // Post Data
-            var postData =  "amount=15&card[number]=4444333322221111&card[expmonth]=12&card[expyear]=23";
-                postData += "&customer[firstname]=Robert&customer[lastname]=Parr&customer[email]=robertp@example.com";
-            var data = Encoding.ASCII.GetBytes(postData);
+			var postData =
+				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+				"<request>" +
+					"<amount>15</amount>" +
+					"<card>" +
+						"<number>4444333322221111</number>" +
+						"<expmonth>12</expmonth>" +
+						"<expyear>23</expyear>" +
+						"<cvv>999</cvv>" +
+					"</card>" +
+					"<customer>" +
+						"<firstname>Robert</firstname>" +
+						"<lastname>Parr</lastname>" +
+						"<email>robertp@example.com</email>" +
+					"</customer>" +
+				"</request>";
+			var data = Encoding.ASCII.GetBytes(postData);
 
-            Console.WriteLine("Creating Request..");
-            // Create Request and set Proper Credentials etc.
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method        = "POST";
-            request.ContentType   = "application/x-www-form-urlencoded";
-            request.ContentLength = data.Length;
-            SetBasicAuth(request, user, key);
+			// Create Request and set Proper Credentials etc.
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method        = "POST";
+			request.ContentType   = "application/xml";
+			request.Accept        = "application/xml";
+			request.ContentLength = data.Length;
+			SetBasicAuth(request, user, key);
 
-            using (var stream = request.GetRequestStream())
-            {
-                stream.Write(data, 0, data.Length);
-            }
+			using (var stream = request.GetRequestStream())
+			{
+				stream.Write(data, 0, data.Length);
+			}
 
-            // receiving response
-            Console.WriteLine("Receiving..");
-            var response = (HttpWebResponse)request.GetResponse();
-            responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+			HttpWebResponse response = null;
 
-            // Write Response
-            Console.WriteLine(responseString); //Uncomment this line to see output via console.
-            Console.WriteLine("Request Finished..");
-            response.Close();
+			try {
+				response = (HttpWebResponse)request.GetResponse();
+			} catch (WebException e) {
+				if (e.Status == WebExceptionStatus.ProtocolError) {
+					response = (HttpWebResponse)e.Response;
+					Console.Write("Errorcode: {0}\n", (int)response.StatusCode);
+				} else {
+					Console.Write("Error: {0}\n", e.Status);
+				}
+			} finally {
+				if (response != null) {
+					string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+					Console.WriteLine(responseString);
+					response.Close();
+				}
+			}
+		}
 
-            // Wait for 'Enter Key'
-            Console.ReadLine();
-        }
-
-        /* Sets our basic auth credentials
-        *
-        *  @params
-        *   WebRequest request   our webrequest object
-        *   String     user      our username
-        *   String     key       our key for auth
-        */
-        static void SetBasicAuth(WebRequest request, String user, String key)
-        {
-            string authInfo = user + ":" + key;
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-            request.Headers["Authorization"] = "Basic " + authInfo;
-        }
-    }
+		/* Sets our basic auth credentials
+		*
+		*  @params
+		*   WebRequest request   our webrequest object
+		*   String     user      our username
+		*   String     key       our key for auth
+		*/
+		static void SetBasicAuth(WebRequest request, String user, String key)
+		{
+			string authInfo = user + ":" + key;
+			authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+			request.Headers["Authorization"] = "Basic " + authInfo;
+		}
+	}
 }
-
